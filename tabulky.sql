0ef665fc7c793630b3c7170043a397f80ca04b46
Kristián Tóth <kikuskomikusko@gmail.com>
2026-09-23 14:13:13 +0200

    Add files via upload

--- tabulky.sql
+++ tabulky.sql
@@ -1,19 +1,20 @@
+create DATABASE superstore;
 create TABLE customer (
-    customer_id INT PRIMARY KEY,
+    customer_id VARCHAR(20) PRIMARY KEY,
     customer_name VARCHAR(100),
     segment VARCHAR(50),
     country VARCHAR(50),
     region VARCHAR(50)
-)
+);
 create TABLE products (
     product_id VARCHAR(20) PRIMARY KEY,
     category VARCHAR(50),
     sub_category VARCHAR(50),
     product_name VARCHAR(100)
-)
+);
 create TABLE orders (
     order_id VARCHAR(20) PRIMARY KEY,
-    customer_id INT,
+    customer_id VARCHAR(20),
     product_id VARCHAR(20),
     order_date DATE,
     ship_date DATE,
@@ -27,3 +28,7 @@ create TABLE orders (
     REFERENCES products(product_id)
 
 );
+
+select * from orders;
+select * from products;
+select * from customer;
