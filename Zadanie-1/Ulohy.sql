a0a90dcf6ba0025869894c25b3e055d2323ab0d2
Kristián Tóth <kikuskomikusko@gmail.com>
2026-09-26 12:33:48 +0200

    Uloha 4

--- Zadanie-1/Ulohy.sql
+++ Zadanie-1/Ulohy.sql
@@ -1,2 +1,3 @@
 SELECT o.order_id, cu.customer_name, o.sales FROM orders o JOIN customer cu ON o.customer_id = cu.customer_id WHERE sales > 500 ORDER BY sales DESC;
 SELECT o.order_id, cu.customer_name, pr.category, o.sales FROM orders o JOIN customer cu ON o.customer_id = cu.customer_id JOIN products pr on o.product_id = pr.product_id;
+SELECT cu.region, SUM(o.sales) as hodnota_predaja FROM customer cu JOIN orders o on cu.customer_id = o.customer_id GROUP BY cu.region;
